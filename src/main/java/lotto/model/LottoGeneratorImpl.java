1ac09f83d1c8f01842980cb331e714fdb5c9ec79
siwanmoon <simon___@naver.com>
2025-11-02 18:19:12 +0900

    feat: 로또 번호 오름차순 정렬기능 추가

--- src/main/java/lotto/model/LottoGeneratorImpl.java
+++ src/main/java/lotto/model/LottoGeneratorImpl.java
@@ -4,6 +4,7 @@ import static lotto.common.Strategy.LOTTO_MIN_NUMBER;
 import static lotto.common.Strategy.LOTTO_MAX_NUMBER;
 import static lotto.common.Strategy.LOTTO_SIZE;
 
+import java.util.Collections;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -11,6 +12,7 @@ public class LottoGeneratorImpl implements LottoGenerator {
 
     public Lotto buyOneLotto() {
         List<Integer> numbers = Randoms.pickUniqueNumbersInRange(LOTTO_MIN_NUMBER, LOTTO_MAX_NUMBER, LOTTO_SIZE);
+        Collections.sort(numbers);
         return new Lotto(numbers);
     }
 }
